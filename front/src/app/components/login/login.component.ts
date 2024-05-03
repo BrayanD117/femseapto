c9a92cee6af465f836bc3119cc90cb477ce7ccfe
BrayanD117 <brayandavid1928@gmail.com>
2024-05-03 14:24:58 -0500

    Login started

--- front/src/app/components/login/login.component.ts
+++ front/src/app/components/login/login.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { PasswordModule } from 'primeng/password';
+import { FloatLabelModule } from 'primeng/floatlabel';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CardModule, PasswordModule, FloatLabelModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  value: string | undefined;
 }
