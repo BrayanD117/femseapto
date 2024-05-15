0af73adee18526efa637bf15ac981dac3b77a401
BrayanD117 <brayandavid1928@gmail.com>
2024-05-15 15:20:27 -0500

    Global whatsapp button

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -5,11 +5,12 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 // Components
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { WhatsappButtonComponent } from './components/whatsapp-button/whatsapp-button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, NavbarComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, NavbarComponent, FooterComponent, WhatsappButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- front/src/app/components/home/home.component.ts
+++ front/src/app/components/home/home.component.ts
@@ -4,12 +4,11 @@ import { Component } from '@angular/core';
 import { CarouselComponent } from '../home/components/carousel/carousel.component';
 import { CreditServiceHomeComponent } from '../home/components/credit-service-home/credit-service-home.component';
 import { SavingServiceHomeComponent } from '../home/components/saving-service-home/saving-service-home.component';
-import { WhatsappButtonComponent } from '../whatsapp-button/whatsapp-button.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CarouselComponent, CreditServiceHomeComponent, SavingServiceHomeComponent, WhatsappButtonComponent],
+  imports: [CarouselComponent, CreditServiceHomeComponent, SavingServiceHomeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
