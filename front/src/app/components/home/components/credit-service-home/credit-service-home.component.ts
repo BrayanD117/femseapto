8f6a884ae87effd70f18ecc613fc8585b684c9aa
juancai0104 <2220201024@estudiantesunibague.edu.co>
2024-05-15 15:28:40 -0500

    Informacion Home

--- front/src/app/components/home/components/credit-service-home/credit-service-home.component.ts
+++ front/src/app/components/home/components/credit-service-home/credit-service-home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-credit-service-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './credit-service-home.component.html',
   styleUrl: './credit-service-home.component.css'
 })
