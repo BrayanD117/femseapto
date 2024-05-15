80bcafd41b9632b3c6f5e4d6af200b1565c0339e
BrayanD117 <brayandavid1928@gmail.com>
2024-05-15 18:03:07 -0500

    whatsapp button with two options

--- front/src/app/components/whatsapp-button/whatsapp-button.component.ts
+++ front/src/app/components/whatsapp-button/whatsapp-button.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { SpeedDialModule } from 'primeng/speeddial';
 
 @Component({
   selector: 'app-whatsapp-button',
   standalone: true,
-  imports: [],
+  imports: [SpeedDialModule],
   templateUrl: './whatsapp-button.component.html',
-  styleUrl: './whatsapp-button.component.css'
+//     `:host ::ng-deep {
+//         .speeddial-circle-demo {
+//             .p-speeddial-quarter-circle {
+//                 &.p-speeddial-direction-up-left {
+//                     right: 0;
+//                     bottom: 0;
+//                 }
+    
+//                 &.p-speeddial-direction-up-right {
+//                     left: 0;
+//                     bottom: 0;
+//                 }
+    
+//                 &.p-speeddial-direction-down-left {
+//                     right: 0;
+//                     top: 0;
+//                 }
+    
+//                 &.p-speeddial-direction-down-right {
+//                     left: 0;
+//                     top: 0;
+//                 }
+//             }
+//         }
+//     }`
+// ],
+  styleUrls: ['./whatsapp-button.component.css', ],
+  encapsulation: ViewEncapsulation.None,
 })
-export class WhatsappButtonComponent {
+export class WhatsappButtonComponent implements OnInit {
+  items: MenuItem[] = [];
+  iconPath = '../../../assets/WhatsApp.svg.webp';
 
+  ngOnInit() {
+    this.items = [
+      {
+          icon: "pi pi-whatsapp",
+          routerLink: ['/fileupload']
+      },
+      {
+          icon: "pi pi-whatsapp",
+          target: '_blank',
+          url: 'https://api.whatsapp.com/send/?phone=573212289646'
+      }
+  ];
+}
 }
