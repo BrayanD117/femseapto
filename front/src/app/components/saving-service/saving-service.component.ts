f445532fc551d3813f9295bcf8d4bced111b891c
BrayanD117 <brayandavid1928@gmail.com>
2024-05-24 17:01:07 -0500

    better saving services

--- front/src/app/components/saving-service/saving-service.component.ts
+++ front/src/app/components/saving-service/saving-service.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-saving-service',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './saving-service.component.html',
   styleUrl: './saving-service.component.css'
 })
 export class SavingServiceComponent {
+  selectedSavingLine: string = 'AHORRO VIVIENDA';
 
+  selectSavingLine(line: string) {
+    this.selectedSavingLine = line;
+  }
 }
