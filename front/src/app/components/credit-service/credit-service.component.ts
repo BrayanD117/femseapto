e3a057ad7a2d91168e1648852ccb55fe310671f3
BrayanD117 <brayandavid1928@gmail.com>
2024-05-27 08:46:54 -0500

    credit services started

--- front/src/app/components/credit-service/credit-service.component.ts
+++ front/src/app/components/credit-service/credit-service.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DividerModule } from 'primeng/divider';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-credit-service',
   standalone: true,
-  imports: [],
+  imports: [DividerModule, CommonModule, RouterLink],
   templateUrl: './credit-service.component.html',
   styleUrl: './credit-service.component.css'
 })
 export class CreditServiceComponent {
+  selectedCreditLine: string = 'CREDITO COMPRA O MEJORAS DE VIVIENDA';
+
+  selectCreditLine(line: string) {
+    this.selectedCreditLine = line;
+  }
 
 }
