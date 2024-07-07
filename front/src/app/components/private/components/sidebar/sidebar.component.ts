0455d57e5bd26617e4870345e51b02bc8ebbb548
Brayan Patiño <brayandavid1928@gmail.com>
2024-07-07 18:51:23 -0500

    Add close session in sidebar

--- front/src/app/components/private/components/sidebar/sidebar.component.ts
+++ front/src/app/components/private/components/sidebar/sidebar.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { LoginService } from '../../../../services/login.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-sidebar',
@@ -11,4 +13,29 @@ import { RouterLink } from '@angular/router';
 })
 export class SidebarComponent {
   @Input() userRole?: number;
+  showLogoutModal = false;
+
+  constructor(
+    private loginService: LoginService,
+    private router: Router,
+    private cookieService: CookieService
+  ) {}
+
+  openLogoutModal() {
+    this.showLogoutModal = true;
+    console.log("Modal abierto");
+  }
+
+  closeLogoutModal() {
+    this.showLogoutModal = false;
+    console.log("Modal cerrado");
+  }
+
+  confirmLogout() {
+    localStorage.removeItem('auth_token');
+    this.cookieService.delete('auth_token');
+    this.loginService.updateAuthStatus(false);
+    this.closeLogoutModal();
+    window.location.pathname = '/login';
+  }
 }
