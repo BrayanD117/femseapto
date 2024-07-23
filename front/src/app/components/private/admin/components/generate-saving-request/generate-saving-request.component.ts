cd49f6f747729c6b4f0ab1e976873a08034f61a6
Brayan Patiño <brayandavid1928@gmail.com>
2024-07-23 15:35:04 -0500

    Update generate saving excel

--- front/src/app/components/private/admin/components/generate-saving-request/generate-saving-request.component.ts
+++ front/src/app/components/private/admin/components/generate-saving-request/generate-saving-request.component.ts
@@ -4,7 +4,9 @@ import { UserService, User } from '../../../../../services/user.service';
 import { NaturalpersonService } from '../../../../../services/naturalperson.service';
 import { CitiesService } from '../../../../../services/cities.service';
 import { SolicitudAhorroService } from '../../../../../services/request-saving.service';
-import { forkJoin } from 'rxjs';
+import { firstValueFrom } from 'rxjs';
+import * as ExcelJS from 'exceljs';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-generate-saving-request',
@@ -15,7 +17,6 @@ import { forkJoin } from 'rxjs';
 export class GenerateSavingRequestComponent implements OnInit {
   @Input() userId: number = 0;
   @Input() idSolicitudAhorro: number = 0;
-  @Input() Quincena: String = '';
 
   nombreCompleto: string = '';
   numeroDocumento: number = 0;
@@ -45,11 +46,9 @@ export class GenerateSavingRequestComponent implements OnInit {
           this.numeroDocumento = Number(user.numeroDocumento);
           this.naturalPersonService.getByUserId(this.userId).subscribe({
             next: (person) => {
-              console.log('PERSON ',person);
               this.celular = person.celular;
               this.citiesService.getById(person.mpioExpDoc).subscribe({
                 next: (city) => {
-                  
                   this.municipioExpedicionDocumento = city.nombre;
                   this.solicitudAhorroService.getById(this.idSolicitudAhorro).subscribe({
                     next: (solicitud) => {
@@ -88,5 +87,57 @@ export class GenerateSavingRequestComponent implements OnInit {
     console.log('Quincena:', this.quincena);
     console.log('Mes:', this.mes);
     console.log('Celular:', this.celular);
+  }
+
+  async generateExcel() {
+    const workbook = new ExcelJS.Workbook();
+    try {
+      await this.loadTemplate(workbook);
+      const worksheet = workbook.getWorksheet(1);
+      
+      if (worksheet) {
+        const texto = [
+          { text: "Yo ", font: { underline: false } },
+          { text: this.nombreCompleto, font: { underline: true } },
+          { text: " Identificado con cédula N° ", font: { underline: false } },
+          { text: this.numeroDocumento.toString(), font: { underline: true } },
+          { text: " de ", font: { underline: false } },
+          { text: this.municipioExpedicionDocumento, font: { underline: true } },
+          { text: ", autorizo a _____________ para descontar de mi salario el valor de $ ", font: { underline: false } },
+          { text: this.valorTotalAhorro.toString(), font: { underline: true } },
+          { text: " quincenalmente, a partir de la ", font: { underline: false } },
+          { text: this.quincena, font: { underline: true } },
+          { text: " quincena de ", font: { underline: false } },
+          { text: this.mes, font: { underline: true } },
+          { text: ".", font: { underline: false } }
+        ];
+
+        worksheet.getCell('B5').value = { richText: texto };
+
+        const buffer = await workbook.xlsx.writeBuffer();
+        const blob = new Blob([buffer], {
+          type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        });
+        saveAs(blob, `Solicitud de Ahorro ${this.numeroDocumento}.xlsx`);
+      } else {
+        console.error('Worksheet is undefined.');
+      }
+    } catch (error) {
+      console.error('Error loading template', error);
+    }
+  }
+
+  async loadTemplate(workbook: ExcelJS.Workbook) {
+    try {
+      const data: ArrayBuffer = await firstValueFrom(
+        this.http.get('/assets/SOLICITAR_AHORRO_NOMINA.xlsx', {
+          responseType: 'arraybuffer',
+        })
+      );
+      await workbook.xlsx.load(data);
+    } catch (error) {
+      console.error('Error reading the template file:', error);
+      throw error;
+    }
   }
 }
