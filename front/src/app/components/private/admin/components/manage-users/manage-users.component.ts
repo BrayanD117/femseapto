72500590a95130512206ac121b565b0ea1c5723c
Brayan Patiño <brayandavid1928@gmail.com>
2024-07-28 13:31:23 -0500

    Update pills in manage users

--- front/src/app/components/private/admin/components/manage-users/manage-users.component.ts
+++ front/src/app/components/private/admin/components/manage-users/manage-users.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-manage-users',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './manage-users.component.html',
   styleUrl: './manage-users.component.css'
 })
