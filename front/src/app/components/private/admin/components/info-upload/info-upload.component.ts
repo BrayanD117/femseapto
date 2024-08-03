4c9c7d85d232a3a0884514cb4e69602f8b4802e0
juancai0104 <2220201024@estudiantesunibague.edu.co>
2024-08-03 10:00:21 -0500

    Change info upload functions

--- front/src/app/components/private/admin/components/info-upload/info-upload.component.ts
+++ front/src/app/components/private/admin/components/info-upload/info-upload.component.ts
@@ -13,34 +13,38 @@ import * as ExcelJS from 'exceljs';
   styleUrls: ['./info-upload.component.css']
 })
 export class InfoUploadComponent {
-  selectedCreditFile: File | null = null;
-  creditMessage: string | null = null;
+  selectedFiles: { [key: string]: File | null } = {
+    credit: null,
+    saving: null,
+    maxAmount: null
+  };
 
-  selectedSavingFile: File | null = null;
-  savingMessage: string | null = null;
-
-  selectedMaxAmountFile: File | null = null;
-  maxAmountMessage: string | null = null;
+  messages: { [key: string]: string | null } = {
+    credit: null,
+    saving: null,
+    maxAmount: null
+  };
 
   constructor(private creditBalanceService: CreditBalanceService,
     private savingBalanceService: SavingBalanceService, private financialInfoService: FinancialInfoService,
   ) {}
 
-  onCreditFileSelected(event: any) {
+  onFileSelected(event: any, type: string) {
     const file: File = event.target.files[0];
     const validTypes = ['text/csv', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'];
 
     if (file && validTypes.includes(file.type)) {
-      this.selectedCreditFile = file;
-      this.creditMessage = null;
+      this.selectedFiles[type] = file;
+      this.messages[type] = null;
     } else {
-      this.creditMessage = 'Please select a valid CSV or XLSX file.';
-      this.selectedCreditFile = null;
+      this.messages[type] = 'Please select a valid CSV or XLSX file.';
+      this.selectedFiles[type] = null;
     }
   }
 
-  async onCreditFileUpload() {
-    if (this.selectedCreditFile) {
+  async onFileUpload(type: string) {
+    const file = this.selectedFiles[type];
+    if (file) {
       const fileReader = new FileReader();
 
       fileReader.onload = async (e: any) => {
@@ -53,145 +57,76 @@ export class InfoUploadComponent {
           const jsonData: any[] = [];
           worksheet.eachRow((row, rowNumber) => {
             if (rowNumber > 1) {
-              const rowData = {
-                numeroDocumento: row.getCell(1).value,
-                idLineaCredito: row.getCell(2).value,
-                cuotaActual: row.getCell(3).value,
-                cuotasTotales: row.getCell(4).value,
-                valorSolicitado: row.getCell(5).value,
-                valorPagado: row.getCell(6).value,
-                valorSaldo: row.getCell(7).value
-              };
+              const rowData = this.extractRowData(type, row);
               jsonData.push(rowData);
               console.log("JSON DATA: ", jsonData);
             }
           });
 
-          this.creditBalanceService.uploadData(jsonData).subscribe(
-            response => {
-              console.log(response);
-              this.creditMessage = 'File uploaded successfully';
-            },
-            error => {
-              console.error("Error: ", error);
-              this.creditMessage = `Failed to upload file: ${error}`;
-            }
-          );
+          this.uploadData(type, jsonData);
         } else {
-          this.creditMessage = 'No valid worksheet found in the file.';
+          this.messages[type] = 'No valid worksheet found in the file.';
         }
       };
 
-      fileReader.readAsArrayBuffer(this.selectedCreditFile);
+      fileReader.readAsArrayBuffer(file);
     }
   }
 
-  onSavingFileSelected(event: any) {
-    const file: File = event.target.files[0];
-    const validTypes = ['text/csv', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'];
-
-    if (file && validTypes.includes(file.type)) {
-      this.selectedSavingFile = file;
-      this.savingMessage = null;
-    } else {
-      this.savingMessage = 'Please select a valid CSV or XLSX file.';
-      this.selectedSavingFile = null;
+  extractRowData(type: string, row: any): any {
+    switch (type) {
+      case 'credit':
+        return {
+          numeroDocumento: row.getCell(1).value,
+          idLineaCredito: row.getCell(2).value,
+          cuotaActual: row.getCell(3).value,
+          cuotasTotales: row.getCell(4).value,
+          valorSolicitado: row.getCell(5).value,
+          valorPagado: row.getCell(6).value,
+          valorSaldo: row.getCell(7).value
+        };
+      case 'saving':
+        return {
+          numeroDocumento: row.getCell(1).value,
+          idLineaAhorro: row.getCell(2).value,
+          valorSaldo: row.getCell(3).value
+        };
+      case 'maxAmount':
+        return {
+          numeroDocumento: row.getCell(1).value,
+          montoMaxAhorro: row.getCell(2).value
+        };
+      default:
+        return {};
     }
   }
 
-  async onSavingFileUpload() {
-    if (this.selectedSavingFile) {
-      const fileReader = new FileReader();
+  uploadData(type: string, data: any[]) {
+    let service;
 
-      fileReader.onload = async (e: any) => {
-        const arrayBuffer = e.target.result;
-        const workbook = new ExcelJS.Workbook();
-        await workbook.xlsx.load(arrayBuffer);
-        const worksheet = workbook.getWorksheet(1);
+    switch (type) {
+      case 'credit':
+        service = this.creditBalanceService;
+        break;
+      case 'saving':
+        service = this.savingBalanceService;
+        break;
+      case 'maxAmount':
+        service = this.financialInfoService;
+        break;
+    }
 
-        if (worksheet) {
-          const jsonData: any[] = [];
-          worksheet.eachRow((row, rowNumber) => {
-            if (rowNumber > 1) {
-              const rowData = {
-                numeroDocumento: row.getCell(1).value,
-                idLineaAhorro: row.getCell(2).value,
-                valorSaldo: row.getCell(3).value
-              };
-              jsonData.push(rowData);
-              console.log("JSON DATA: ", jsonData);
-            }
-          });
-
-          this.savingBalanceService.uploadData(jsonData).subscribe(
-            response => {
-              console.log(response);
-              this.savingMessage = 'File uploaded successfully';
-            },
-            error => {
-              console.error("Error: ", error);
-              this.creditMessage = `Failed to upload file: ${error}`;
-            }
-          );
-        } else {
-          this.savingMessage = 'No valid worksheet found in the file.';
+    if (service) {
+      service.uploadData(data).subscribe(
+        response => {
+          console.log(response);
+          this.messages[type] = 'File uploaded successfully';
+        },
+        error => {
+          console.error("Error: ", error);
+          this.messages[type] = `Failed to upload file: ${error}`;
         }
-      };
-
-      fileReader.readAsArrayBuffer(this.selectedSavingFile);
-    }
-  }
-
-  onMaxAmountFileSelected(event: any) {
-    const file: File = event.target.files[0];
-    const validTypes = ['text/csv', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'];
-
-    if (file && validTypes.includes(file.type)) {
-      this.selectedMaxAmountFile = file;
-      this.maxAmountMessage = null;
-    } else {
-      this.maxAmountMessage = 'Please select a valid CSV or XLSX file.';
-      this.selectedMaxAmountFile = null;
-    }
-  }
-
-  async onMaxAmountFileUpload() {
-    if (this.selectedMaxAmountFile) {
-      const fileReader = new FileReader();
-
-      fileReader.onload = async (e: any) => {
-        const arrayBuffer = e.target.result;
-        const workbook = new ExcelJS.Workbook();
-        await workbook.xlsx.load(arrayBuffer);
-        const worksheet = workbook.getWorksheet(1);
-
-        if (worksheet) {
-          const jsonData: any[] = [];
-          worksheet.eachRow((row, rowNumber) => {
-            if (rowNumber > 1) {
-              const rowData = {
-                numeroDocumento: row.getCell(1).value,
-                montoMaxAhorro: row.getCell(2).value
-              };
-              jsonData.push(rowData);
-              console.log("JSON DATA: ", jsonData);
-            }
-          });
-
-          this.financialInfoService.uploadData(jsonData).subscribe(
-            response => {
-              this.maxAmountMessage = 'File uploaded successfully';
-            },
-            error => {
-              this.maxAmountMessage = `Failed to upload file: ${error}`;
-            }
-          );
-        } else {
-          this.maxAmountMessage = 'No valid worksheet found in the file.';
-        }
-      };
-
-      fileReader.readAsArrayBuffer(this.selectedMaxAmountFile);
+      );
     }
   }
 }
