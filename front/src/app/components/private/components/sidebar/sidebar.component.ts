bb9b68b76fc30015a81923948b587adf4a12c752
BrayanD117 <brayandavid1928@gmail.com>
2024-09-19 11:30:04 -0500

    Fix on sidebar logout

--- front/src/app/components/private/components/sidebar/sidebar.component.ts
+++ front/src/app/components/private/components/sidebar/sidebar.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
 import { LoginService } from '../../../../services/login.service';
 import { CookieService } from 'ngx-cookie-service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
@@ -11,8 +12,10 @@ import { CookieService } from 'ngx-cookie-service';
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit, OnDestroy {
   @Input() userRole?: number;
+  private authListenerSubs: Subscription | undefined;
+  isAuthenticated = false;
 
   constructor(
     private loginService: LoginService,
@@ -20,10 +23,42 @@ export class SidebarComponent {
     private cookieService: CookieService
   ) {}
 
+  ngOnInit() {
+    this.authListenerSubs = this.loginService
+      .getAuthStatusListener()
+      .subscribe((isAuthenticated) => {
+        this.isAuthenticated = isAuthenticated;
+      });
+  }
+
+  ngOnDestroy() {
+    if (this.authListenerSubs) {
+      this.authListenerSubs.unsubscribe();
+    }
+  }
+
   confirmLogout() {
     localStorage.removeItem('auth_token');
     this.cookieService.delete('auth_token');
-    this.loginService.updateAuthStatus(false);
-    this.router.navigate(['/login']);
+    
+    if (!this.cookieService.check('auth_token')) {
+      this.loginService.updateAuthStatus(false);
+      this.router.navigate(['/login']);
+    } else {
+      console.error('Error al eliminar la cookie');
+    }
+  }
+
+  handleLogout() {
+    this.confirmLogout();
+    this.closeModal();
+  }
+
+  closeModal() {
+    const modalElement = document.getElementById('logoutModal');
+    if (modalElement) {
+      const modalInstance = new (window as any).bootstrap.Modal(modalElement);
+      modalInstance.hide();
+    }
   }
 }
