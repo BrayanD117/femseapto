7857377124c350d7348797edd703a468cb46da87
juancai0104 <2220201024@estudiantesunibague.edu.co>
2024-12-11 09:59:53 -0500

    Add get media info of user request

--- back/api/usuarioscomunicacion.php
+++ back/api/usuarioscomunicacion.php
@@ -35,6 +35,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         http_response_code(500);
         echo json_encode(["message" => "Ocurrió un error al sincronizar los datos.", "error" => $e->getMessage()]);
     }
+} elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    if (isset($_GET['idUsuario'])) {
+        $id = $_GET['idUsuario'];
+        $usuarioComunicaciones = $controlador->obtenerPorIdUsuario($id);
+        if ($usuarioComunicaciones) {
+            header('Content-Type: application/json');
+            echo json_encode($usuarioComunicaciones);
+        } else {
+            http_response_code(404);
+            echo json_encode(array("message" => "Persona no encontrada."));
+        }
+    } else {
+        http_response_code(400);
+        echo json_encode(array("message" => "ID de usuario no proporcionado."));
+    }
 } else {
     http_response_code(405);
     echo json_encode(["message" => "Método no permitido."]);
